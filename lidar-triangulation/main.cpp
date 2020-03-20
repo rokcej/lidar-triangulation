d8fb1be02a5e0a8a0547b5615f487716f4cf517d
rokcej <rokcej1997@gmail.com>
2020-03-20 16:39:50 +0100

    Made some progress on structures

--- lidar-triangulation/main.cpp
+++ lidar-triangulation/main.cpp
@@ -7,7 +7,10 @@
 #include "lasreader.hpp"
 #include "laswriter.hpp"
 
-#define DEBUG false
+#define DEBUG true
+
+inline int M1(const int& x) { return (x + 1) % 3; }
+inline int M2(const int& x) { return (x + 2) % 3; }
 
 int main() {
 	// Read LiDAR data
@@ -15,7 +18,7 @@ int main() {
 	lasreadopener.set_file_name("GK_462_100.laz");
 	LASreader* lasreader = lasreadopener.open();
 
-	int numPoints = (int)lasreader->npoints;
+	int numPoints = 10000; // (int)lasreader->npoints;
 	Point* points = new Point[numPoints];
 
 	for (int i = 0; i < numPoints && lasreader->read_point(); ++i) {
@@ -27,6 +30,10 @@ int main() {
 
 	lasreader->close();
 	delete lasreader;
+
+	for (int i = 0; i < numPoints; ++i) {
+
+	}
 
 	// Get min, max and span
 	double xMin = points[0].x, xMax = points[0].x;
@@ -63,15 +70,15 @@ int main() {
 	// Test bounding triangle
 	if (DEBUG) {
 		for (int i = 0; i < numPoints; ++i) {
-			double w0, w1, w2;
-			if (!triBound.isInside(&points[i], w0, w1, w2) || w0 == 0.0 || w1 == 0.0 || w2 == 0.0) {
-				if (w0 == 0.0 || w1 == 0.0 || w2 == 0.0)
+			double w[3];
+			if (!triBound.isInside(&points[i], w) || w[0] == 0.0 || w[1] == 0.0 || w[2] == 0.0) {
+				if (w[0] == 0.0 || w[1] == 0.0 || w[2] == 0.0)
 					std::cout << "Point on the edge of bounding triangle!" << std::endl;
 				else
 					std::cout << "Point outside bounding triangle!" << std::endl;
 
 				std::cout << "Point: " << points[i].x << ", " << points[i].y << std::endl;
-				std::cout << "Baricentric weights: " << w0 << ", " << w1 << ", " << w2 << std::endl;
+				std::cout << "Baricentric weights: " << w[0] << ", " << w[1] << ", " << w[2] << std::endl;
 				std::cout << "Triangle point 1: " << triBound.p0->x << ", " << triBound.p0->y << std::endl;
 				std::cout << "Triangle point 2: " << triBound.p1->x << ", " << triBound.p1->y << std::endl;
 				std::cout << "Triangle point 3: " << triBound.p2->x << ", " << triBound.p2->y << std::endl;
@@ -79,6 +86,69 @@ int main() {
 				return 1;
 			}
 		}
+	}
+
+	// Delaunay tree (DT)
+	Node* root = new Node(&p0Bound, &p1Bound, &p2Bound);
+
+	// Incrementally add points to DT
+	for (int iPoint = 0; iPoint < numPoints; ++iPoint) {
+		// Find triangle in which the point lies
+		Point* p = &points[iPoint];
+		Node* n = root;
+		double w[3];
+		while (n->hasChildren) {
+			if (n->child[0] != nullptr && n->child[0]->tri.isInside(p, w)) n = n->child[0];
+			else if (n->child[1] != nullptr && n->child[1]->tri.isInside(p, w)) n = n->child[1];
+			else if (n->child[2] != nullptr && n->child[2]->tri.isInside(p, w)) n = n->child[2];
+			else {
+				std::cout << "ERROR: Point not inside any triangle!" << std::endl;
+				std::cout << iPoint << std::endl;
+				std::cout << w[0] << ", " << w[1] << ", " << w[2] << std::endl;
+				return 1;
+			}
+		}
+
+		// Add point to triangle
+		if (n == root || w[0] != 0. && w[1] != 0. && w[2] != 0.)  { // Point inside of triangle
+			for (int i = 0; i < 3; ++i) {
+				n->child[i] = new Node(p, n->tri.p[M1(i)], n->tri.p[M2(i)]);
+			}
+
+			/*n->child[0] = new Node(p, n->tri.p1, n->tri.p2);
+			n->child[1] = new Node(p, n->tri.p2, n->tri.p0);
+			n->child[2] = new Node(p, n->tri.p0, n->tri.p1);*/
+
+			for (int i = 0; i < 3; ++i) {
+				n->child[i]->neighbor[0] = n->neighbor[i];
+				n->child[i]->neighbor[1] = n->neighbor[M1(i)];
+				n->child[i]->neighbor[2] = n->neighbor[M2(i)];
+			}
+
+			/*n->child[0]->neighbor[0] = n->neighbor[0];
+			n->child[0]->neighbor[1] = n->child[1];
+			n->child[0]->neighbor[2] = n->child[2];
+
+			n->child[1]->neighbor[0] = n->neighbor[1];
+			n->child[1]->neighbor[1] = n->child[2];
+			n->child[1]->neighbor[2] = n->child[0];
+
+			n->child[2]->neighbor[0] = n->neighbor[2];
+			n->child[2]->neighbor[1] = n->child[0];
+			n->child[2]->neighbor[2] = n->child[1];*/
+
+			for (int i = 0; i < 3; ++i) {
+				n->child[i]->updateNeigbors(n);
+			}
+
+		} else { // Point on edge of triangle
+			int edge = 0;
+			if (w[1] == 0.) edge = 1;
+			else if (w[2] == 0.) edge = 2;
+
+			n->split2(p, edge);
+		}
+		n->hasChildren = true;
 	}
 
 	return 0;
--- lidar-triangulation/structures.h
+++ lidar-triangulation/structures.h
@@ -7,19 +7,80 @@ struct Point {
 
 class Triangle {
 public:
+	Point* p[3];
 	Point *p0, *p1, *p2;
 	double area; // Signed area
 
-	Triangle(Point *p0, Point *p1, Point *p2) : p0{ p0 }, p1 { p1 }, p2{ p2 } {
+	Triangle(Point* p0, Point* p1, Point* p2) : p{ p0, p1, p2 }, p0 { p0 }, p1{ p1 }, p2{ p2 } {
 		this->area = 0.5f * (-p1->y * p2->x + p0->y * (-p1->x + p2->x) + p0->x * (p1->y - p2->y) + p1->x * p2->y);
 	}
+	Triangle(Triangle* t) : Triangle(t->p0, t->p1, t->p2) {}
 
-	int isInside(Point *p, double &w0, double &w1, double &w2) { // https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle
+	int isInside(Point *p, double *w) { // https://stackoverflow.com/questions/2049582/how-to-determine-if-a-point-is-in-a-2d-triangle
 		double sign = area >= 0 ? +1. : -1.;
-		w1 = sign * (float)(p0->y * p2->x - p0->x * p2->y + (p2->y - p0->y) * p->x + (p0->x - p2->x) * p->y);
-		w2 = sign * (float)(p0->x * p1->y - p0->y * p1->x + (p0->y - p1->y) * p->x + (p1->x - p0->x) * p->y);
-		w0 = (2.f * area * sign) - w1 - w2;
+		w[1] = sign * (float)(p0->y * p2->x - p0->x * p2->y + (p2->y - p0->y) * p->x + (p0->x - p2->x) * p->y);
+		w[2] = sign * (float)(p0->x * p1->y - p0->y * p1->x + (p0->y - p1->y) * p->x + (p1->x - p0->x) * p->y);
+		w[0] = (2.f * area * sign) - w[1] - w[2];
 		
-		return w0 >= 0. && w1 >= 0. && w2 >= 0.;
+		return w[0] >= 0. && w[1] >= 0. && w[2] >= 0.;
+	}
+};
+
+class Node {
+public:
+	Triangle tri;
+	bool hasChildren = false;
+	Node* child[3] = { nullptr };
+	Node* neighbor[3] = { nullptr };
+
+	Node(Point* p0, Point* p1, Point* p2) : tri{ p0, p1, p2 } {}
+
+	void split2(Point* p, int edge) {
+		this->_split2(p, edge);
+
+		if (neighbor[edge] != nullptr) {
+			int edgeNeigh = 0;
+			if (neighbor[edge]->neighbor[1] == this)
+				edgeNeigh = 1;
+			else if (neighbor[edge]->neighbor[2] == this)
+				edgeNeigh = 2;
+
+			neighbor[edge]->_split2(p, edgeNeigh);
+
+			child[0]->neighbor[2] = neighbor[edge]->child[1];
+			child[1]->neighbor[1] = neighbor[edge]->child[0];
+			neighbor[edge]->child[0]->neighbor[2] = child[1];
+			neighbor[edge]->child[1]->neighbor[1] = child[0];
+		}
+	}
+
+	void updateNeigbors(Node *parent) { // Make sure neighbours are connected back to this node instead of its parent
+		for (int in = 0; in < 3; ++in) {
+			if (neighbor[in] != nullptr) {
+				for (int inn = 0; inn < 3; ++inn) {
+					if (neighbor[in]->neighbor[inn] == parent) {
+						neighbor[in]->neighbor[inn] = this;
+						break;
+					}
+				}
+			}
+		}
+	}
+
+private:
+	void _split2(Point* p, int edge) {
+		int i0 = edge, i1 = (edge + 1) % 3, i2 = (edge + 2) % 3;
+		child[0] = new Node(p, tri.p[i2], tri.p[i0]);
+		child[1] = new Node(p, tri.p[i0], tri.p[i1]);
+
+		child[0]->neighbor[0] = neighbor[i1];
+		child[0]->neighbor[1] = child[1];
+		child[1]->neighbor[0] = neighbor[i2];
+		child[1]->neighbor[2] = child[0];
+
+		child[0]->updateNeigbors(this);
+		child[1]->updateNeigbors(this);
+
+		this->hasChildren = true;
 	}
 };
